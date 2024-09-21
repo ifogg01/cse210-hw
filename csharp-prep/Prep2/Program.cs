92d1f19839332c4ca6c80ab7c1b4d9eabb8fc312
ifogg01 <ifogg@byui.edu>
2024-09-20 19:20:15 -0600

    Prep2 submission

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,52 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string input = Console.ReadLine();
+        
+        double gradePercentage;
+        string letter = ""; 
+
+        if (double.TryParse(input, out gradePercentage))
+        {
+            if (gradePercentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            Console.WriteLine($"Your letter grade is: {letter}");
+
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine("Good Job! You passed.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, but you did not pass. Don't give up, try again.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error. Please enter a valid numeric grade percentage from 0-100.");
+        }
     }
 }
+
