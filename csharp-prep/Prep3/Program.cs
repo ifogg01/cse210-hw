cbac38147c93103b78af04d51c2ee77f8ea0d428
ifogg01 <ifogg@byui.edu>
2024-09-20 19:40:50 -0600

    Prep 3 submission

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+        int guess = -1;  
+        while (guess != magicNumber)
+        {
+            Console.Write("Can yo guess the magic number (between 1 and 100): ");
+            string guessInput = Console.ReadLine();
+
+            if (int.TryParse(guessInput, out guess))
+            {
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Sorry too low! Try guessing higher.");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Sorry too high! Try guessing lower.");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error. Please enter a valid number.");
+            }
+        }
     }
 }
