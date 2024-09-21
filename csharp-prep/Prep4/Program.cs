3a5bd2289a9c80557555090bfffa2b2bb86682ce
ifogg01 <ifogg@byui.edu>
2024-09-21 17:56:16 -0600

    finish prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+        
+        do
+        {
+            Console.Write("Please enter any number (Enter 0 to quit): ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+
+        } while (input != 0);
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            Console.WriteLine("Sum = " + sum);
+
+            double average = numbers.Average();
+            Console.WriteLine("Average = " + average);
+
+            int max = numbers.Max();
+            Console.WriteLine("Max = " + max);
+        }
+        else
+        {
+            Console.WriteLine("Error. No numbers were entered.");
+        }
     }
 }
