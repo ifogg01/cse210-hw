1cb9ea767fedbc97725bc303df217e4bbc6cafd9
ifogg01 <ifogg@byui.edu>
2024-09-23 21:46:35 -0600

    Week2Prepare submission

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Clincal R&D";
+        job1._company = "TEVA";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "R&D Production";
+        job2._company = "Watson";
+        job2._startYear = 2012;
+        job2._endYear = 2019;
+
+        Resume myResume = new Resume();
+        myResume._name = "Isaac Fogg";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
