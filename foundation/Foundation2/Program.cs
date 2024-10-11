f3cbdf7316b6233ae73d1f26ec43b123e84f3f6d
ifogg01 <ifogg@byui.edu>
2024-10-11 11:16:50 -0600

    fix

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -9,7 +9,6 @@ public class Product
     private double _price;
     private int _quantity;
 
-    
     public Product(string name, string productId, double price, int quantity)
     {
         _name = name;
