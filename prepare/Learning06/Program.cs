4d9e2ea43703271dca7fd1ab13fa3b04911025de
ifogg01 <ifogg@byui.edu>
2024-10-21 21:03:28 -0600

    W6-LearningActivitySubmit

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+         Console.WriteLine("");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 4);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 2, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Blue", 3);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+            Console.WriteLine("");
+
+        }
     }
 }
